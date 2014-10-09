218ebc8925bf6cccaad493b492e7993e0be71c89
Eric Benoit <benwa02@gmail.com>
2014-10-09 19:45:11 -0400

    Added 3 new methods to use food object. Finished exercise.

--- food.rb
+++ food.rb
@@ -1,14 +1,15 @@
 class Food
-	attr_accessor :name, :noise, :eaten
+	attr_accessor :name, :noise, :days_old, :eaten
 
-	def initialize(name, noise)
+	def initialize(name, noise, days_old)
 		@name = name
 		@noise = noise
+		@days_old = days_old
 		@eaten = false
 	end
 
 	def food_info
-		"#{@name} + #{@noise} + #{eaten}"
+		"#{@name} + #{@noise} + #{@eaten}"
 	end
 
 end
--- user.rb
+++ user.rb
@@ -17,14 +17,32 @@ class User
 		"#{@first_name} #{@last_name}"
 	end
 
-	def eat(test)
-		# puts "Eating noise is #{noise} for #{name}."
-		puts "putting name and noise here"
-		puts "#{@name}"
-		test.noise = "nom nom"
-		puts test.noise
-		puts "-------"
-		test.eaten = true
+	def eat(food)
+		puts "#{first_name} just ate #{food.name}."
+		puts food.noise
+		food.eaten = true
 		@meals_eaten += 1
+	end
+
+	def is_rotten(food)
+		if food.days_old > 14
+			puts "#{first_name}, your #{food.name} is rotten."
+		else
+			puts "#{first_name}, your #{food.name} is still edible."
+		end
+	end
+
+	def food_prep(food)
+		if food.name == "Banana"
+			puts "Just peel the #{food.name}."
+		elsif food.name == "grapes"
+			puts "Just pluck the #{food.name} off the vine."
+		else
+			puts "Hmmm, haven't eaten that before. Not sure how to prepare #{food.name}"
+		end
+	end
+
+	def blender(food_1, food_2, food_3)
+		puts "#{food_1.name}-#{food_2.name}-#{food_3.name} pizza is disgusting."
 	end
 end
